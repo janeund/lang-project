b484fd7b320389c887ef594e404a34a4bacb9b21
Evgeniia <maongaku@gmail.com>
2026-08-26 12:44:54 +0200

    feat: persist phrases in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import PhraseList from "./components/PhraseList"
 import { initialPhrases } from "./data/phrases"
 import type { Phrase } from "./types"
 import AddPhraseForm from "./components/AddPhraseForm"
 
 function App() {
-  const [ phrases, setPhrases ] = useState<Phrase[]>(initialPhrases)
+  const [ phrases, setPhrases ] = useState<Phrase[]>(() => {
+    const saved = localStorage.getItem('phrases')
+    return saved ? JSON.parse(saved) : initialPhrases
+  })
 
   function handleAddPhrase(newPhrase: Phrase) {
     setPhrases([...phrases, newPhrase])
   }
+
+  useEffect(() => {
+    localStorage.setItem('phrases', JSON.stringify(phrases))
+  },[phrases])
 
   return (
     <>
